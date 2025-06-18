f8de4cec10e3e7ecbaf410eb961565d84e32b76d
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2025-06-18 19:35:46 +0200

    update type definitions

--- mergeClassNames.d.ts
+++ mergeClassNames.d.ts
@@ -2,4 +2,3 @@ export declare const mergeClassNames: (...args: (string | false)[]) => string;
 export declare const mergeClassNamesDebugger: (
     ...args: (string | false)[]
 ) => string;
-export declare const isEmptyString: (argument: string) => boolean;
